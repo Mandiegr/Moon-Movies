e74db32fb1cf1faf31a8636e2bb9d2a596afcded
Mandiegr <nannda076@gmail.com>
2024-11-29 14:20:59 -0300

    update moviesitem

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import styles from "../views/Movies.module.css"
+import styles from "../views/Styles/Movies.module.css"
 export function MovieItem({movie}) {
     return (
         <div className="">
