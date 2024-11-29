872c6e7c70d495bafb2867fb1e3df50d5ba7962b
Mandiegr <nannda076@gmail.com>
2024-11-29 12:08:51 -0300

    feat: Initial components and package update

---
+++ src/routes.js
@@ -0,0 +1,26 @@
+import { RootLayout } from "./views/RootLayout";
+import { Home } from "./views/Home";
+import { Movies } from "./views/Movies";
+import { MovieDetails } from "./views/MoveDetails";
+import { createBrowserRouter } from "react-router-dom";
+import Genre from "./views/Genres";
+
+export const routes =  createBrowserRouter([
+
+    {
+        path:"/",
+        element: <RootLayout/>,
+        children: [
+            {
+            path: "/",
+            element: <Home/>,
+            },
+
+            {
+                path:"/movies",
+                element: <Movies/>
+            },
+         
+        ]
+    }
+])
